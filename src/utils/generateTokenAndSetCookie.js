b89593988d756362f8a63804256c5d32d7f60641
youssef-elmelegy <youssefelmelegy999@gmail.com>
2025-01-07 23:23:24 +0200

    fix: cookie locally

--- src/utils/generateTokenAndSetCookie.js
+++ src/utils/generateTokenAndSetCookie.js
@@ -7,10 +7,9 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    path: "/",
   });
 
   return token;
