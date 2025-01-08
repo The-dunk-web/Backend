a56e34ca0a1765b2cb3373154ac5b2cbe169916a
youssef-elmelegy <youssefelmelegy999@gmail.com>
2025-01-08 12:57:02 +0200

    feat: password reset template

--- src/utils/sendEmails.js
+++ src/utils/sendEmails.js
@@ -1,11 +1,14 @@
 import transporter from "../config/nodeMailer.config.js";
+import { passwordResetTemplate } from "./EmailTemplates/passwordReset.js";
 
 export const sendPasswordResetEmail = async (email, link) => {
+  const htmlTemplate = passwordResetTemplate(link);
+
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
     subject: "Password Reset Link",
-    text: `Click on the link to reset your password: ${link}`,
+    html: htmlTemplate,
   };
 
   await transporter.sendMail(mailOptions);
