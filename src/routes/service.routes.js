35b428d6d5e3390f9fa9aeea1ea9003315f39353
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-25 03:54:21 +0200

    fix: fix delete service route

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -1,25 +1,25 @@
-import express from "express";
-import upload from "../middleware/multer.profile.js";
+import express from 'express';
+import upload from '../middleware/multer.profile.js';
 import {
   createService,
   getAllServices,
   getServiceById,
   updateService,
   deleteService,
-} from "../controllers/service.controllers.js";
-import { verifyToken } from "../middleware/verifyToken.js";
+} from '../controllers/service.controllers.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
-router.post("/create", verifyToken, upload.array("photos", 5), createService);
-router.get("/", getAllServices);
-router.get("/:id", getServiceById);
+router.post('/create', verifyToken, upload.array('photos', 5), createService);
+router.get('/', getAllServices);
+router.get('/:id', getServiceById);
 router.put(
-  "/update/:id",
+  '/update/:id',
   verifyToken,
-  upload.array("photos", 5),
+  upload.array('photos', 5),
   updateService
 );
-router.delete("/:id", verifyToken, deleteService);
+router.delete('/delete/:id', verifyToken, deleteService);
 
 export default router;
